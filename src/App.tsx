bfae48ccce8dafc33895a8ea49a518f10461e8ea
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-09-29 15:25:21 +0600

    element movement on x and y axis using translate

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="main">
+      <div className="box-1"></div>
+      <h1>Something cool</h1>
+    </div>
+  );
+};
+
+export default App;
