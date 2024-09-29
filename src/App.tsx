043733704583bad48f9067168340c181274bfe03
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-09-29 19:06:10 +0600

    CSS Clip path transition

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import "./App.css";
 const App = () => {
   return (
     <div className="main">
-      <div className="loading">
-        <div className="glass"></div>
-        <div className="box"></div>
+      <div className="card">
+        <h1>Hello, How're you doing today</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium quod at aspernatur porro mollitia magni voluptatem dolorem similique, quam voluptas veritatis dolore. Labore recusandae sequi deserunt eos ea amet nemo culpa, numquam quas fugit dolore blanditiis eum error quisquam eaque ab a delectus repellat modi impedit laudantium voluptates saepe reprehenderit.</p>
       </div>
     </div>
   );
