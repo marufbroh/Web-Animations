c405d7aa49808824845674902a50fae211ae41a4
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-09-29 15:47:46 +0600

    transform-origin, rotate() skew() scale()

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ const App = () => {
   return (
     <div className="main">
       <div className="box-1"></div>
-      <h1>Something cool</h1>
+      <div className="box-container-2">
+        <div className="box-2"></div>
+      </div>
     </div>
   );
 };
