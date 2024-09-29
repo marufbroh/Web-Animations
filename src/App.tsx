0e7b98318dd04eb0d32156bd3cbc9dd546c1105c
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-09-29 19:18:46 +0600

    CSS transition mixed with JavaScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import "./App.css";
 
 const App = () => {
+  const name = "Programming Moye";
+
+  const nameArray = name.split("");
+
   return (
     <div className="main">
-      <div className="card">
-        <h1>Hello, How're you doing today</h1>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium quod at aspernatur porro mollitia magni voluptatem dolorem similique, quam voluptas veritatis dolore. Labore recusandae sequi deserunt eos ea amet nemo culpa, numquam quas fugit dolore blanditiis eum error quisquam eaque ab a delectus repellat modi impedit laudantium voluptates saepe reprehenderit.</p>
+      <div className="container">
+        {nameArray.map((item, i) => (
+           <span className="alphabet" style={{transitionDelay: `${i * 30}ms`}}> {item}</span>
+        ))}
       </div>
     </div>
   );
