e7646bae3ffd2e5085612e1458bb4dc227b57604
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-09-30 08:06:08 +0600

    Motion Element

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,10 @@
 import "./App.css";
+import Lesson1 from "./components/Lesson1";
 
 const App = () => {
-  const name = "Programming Moye";
-
-  const nameArray = name.split("");
-
   return (
-    <div className="main">
-      <div className="container">
-        {nameArray.map((item, i) => (
-           <span className="alphabet" style={{transitionDelay: `${i * 30}ms`}}> {item}</span>
-        ))}
-      </div>
+    <div className="flex min-h-screen justify-center items-center">
+      <Lesson1 />
     </div>
   );
 };
