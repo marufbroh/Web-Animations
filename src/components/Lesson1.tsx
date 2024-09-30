490766004c5130702cfea0b4b20ae490f19bcd6b
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-09-30 09:25:52 +0600

    Gesture - Drag

--- src/components/Lesson1.tsx
+++ src/components/Lesson1.tsx
@@ -1,39 +1,48 @@
 import { motion } from "framer-motion";
+import { useRef } from "react";
 
 const parent = {
   hidden: { opacity: 0, scale: 0.9 },
   visible: {
-    opacity: 1,
+    opacity: 0.6,
     scale: 1,
     transition: {
       ease: "easeInOut",
-      duration: 1.5,
-    },
-  },
-  hover: {
-    scale: 1.1,
-    transition: {
       duration: 0.5,
     },
   },
+  hover: {
+    opacity: 1,
+    transition: {
+      ease: "easeInOut",
+      duration: 1,
+    },
+  },
   tap: {
-    scale: 1,
-    rotate: 45,
+    scale: 1.1,
+    boxShadow: "0px 10px 10px",
   },
 };
 
 const Lesson1 = () => {
+  const parentRef = useRef(null);
   return (
-    <motion.div
-      className="size-64 bg-indigo-500 rounded-lg flex flex-wrap gap-5 p-5 justify-center items-center"
-      variants={parent}
-      initial="hidden"
-      animate="visible"
-      whileHover="hover"
-      whileTap="tap"
-      onHoverStart={() => console.log("Hovered")}
-      onHoverEnd={() => console.log("Hover Ended")}
-    ></motion.div>
+    <div
+      ref={parentRef}
+      className="border border-red-500 size-[500px] flex justify-center items-center"
+    >
+      <motion.div
+        className="size-64 bg-indigo-500 rounded-lg flex flex-wrap gap-5 p-5 justify-center items-center"
+        variants={parent}
+        initial="hidden"
+        animate="visible"
+        whileHover="hover"
+        drag
+        dragElastic={0.7}
+        dragConstraints={parentRef}
+        whileDrag= "tap"
+      ></motion.div>
+    </div>
   );
 };
 
