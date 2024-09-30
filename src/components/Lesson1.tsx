b34f111516718d8cbf97c741bf7715ed7dfe88dd
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-09-30 08:39:35 +0600

    Variants and transition orchestration (stagger animation)

--- src/components/Lesson1.tsx
+++ src/components/Lesson1.tsx
@@ -1,13 +1,48 @@
 import { motion } from "framer-motion";
 
+const parent = {
+  hidden: { opacity: 0, scale: 0.9 },
+  visible: { opacity: 1, scale: 1 },
+};
+
+const child = {
+  hidden: { opacity: 0, scale: 0.1 },
+  visible: { opacity: 1, scale: 1 },
+};
+
 const Lesson1 = () => {
   return (
     <motion.div
-      className="size-64 bg-indigo-500 rounded-lg"
-      initial={{ rotate: 0 }}
-      animate={{ rotate: 360 }}
-      transition={{ type: "spring", duration: 2 }}
-    ></motion.div>
+      className="size-64 bg-indigo-500 rounded-lg flex flex-wrap gap-5 p-5 justify-center items-center"
+      variants={parent}
+      initial="hidden"
+      animate="visible"
+      transition={{
+        ease: "easeInOut",
+        duration: 1.5,
+        // repeat: Infinity,
+        repeatType: "reverse",
+        delayChildren: 1,
+        staggerChildren: 0.5,
+      }}
+    >
+      <motion.div
+        className="size-20 bg-cyan-400 rounded-sm"
+        variants={child}
+      ></motion.div>
+      <motion.div
+        className="size-20 bg-cyan-400 rounded-sm"
+        variants={child}
+      ></motion.div>
+      <motion.div
+        className="size-20 bg-cyan-400 rounded-sm"
+        variants={child}
+      ></motion.div>
+      <motion.div
+        className="size-20 bg-cyan-400 rounded-sm"
+        variants={child}
+      ></motion.div>
+    </motion.div>
   );
 };
 
