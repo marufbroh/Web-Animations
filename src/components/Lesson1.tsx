95ac49acecaf64f9c5ea3f6a34f92d8696021a32
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-10-01 11:48:10 +0600

    Framer Motion Hook - useAnimationControl()

--- src/components/Lesson1.tsx
+++ src/components/Lesson1.tsx
@@ -1,52 +1,30 @@
-import { motion } from "framer-motion";
-import { useRef } from "react";
-
-const parent = {
-  hidden: { x: 0, y: 0, opacity: 0 },
-  visible: {
-    x: [0, 300, -300, 0],
-    y: [0, 300, -300, 0],
-    rotate: [0, 300, -300, 0],
-    opacity: 1,
-    transition: {
-      ease: "linear",
-      repeat: Infinity,
-      duration: 5,
-      opacity: {
-        duration: 0.5,
-      },
-    },
-  },
-  hover: {
-    opacity: 1,
-    transition: {
-      ease: "easeInOut",
-      duration: 1,
-    },
-  },
-  tap: {
-    scale: 1.1,
-    boxShadow: "0px 10px 10px",
-  },
-};
+import { motion, useAnimationControls } from "framer-motion";
+import { useEffect } from "react";
 
 const Lesson1 = () => {
-  const parentRef = useRef(null);
+  const controls = useAnimationControls();
+
+  useEffect(() => {
+controls.start((i) => ({x: 200, transition: {delay: i * 1}}))
+  }, [])
+
   return (
-    <div
-      ref={parentRef}
-      className="border border-red-500 size-[400px] flex justify-center items-center"
-    >
+    <div className="border border-red-500 size-[400px] flex flex-col justify-center items-center">
       <motion.div
         className="size-32 bg-indigo-500 rounded-lg flex flex-wrap gap-5 p-5 justify-center items-center"
-        variants={parent}
-        initial="hidden"
-        animate="visible"
-        whileHover="hover"
-        drag
-        dragElastic={0.7}
-        dragConstraints={parentRef}
-        whileDrag="tap"
+        animate={controls}
+        custom={1}
+        onClick={() => controls.stop()}
+      ></motion.div>
+      <motion.div
+        className="size-32 bg-indigo-500 rounded-lg flex flex-wrap gap-5 p-5 justify-center items-center"
+        animate={controls}
+        custom={2}
+      ></motion.div>
+      <motion.div
+        className="size-32 bg-indigo-500 rounded-lg flex flex-wrap gap-5 p-5 justify-center items-center"
+        animate={controls}
+        custom={3}
       ></motion.div>
     </div>
   );
