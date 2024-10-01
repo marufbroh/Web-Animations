2a6807a4d18c99ce559fe1ff4ad15dbd554f356b
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-10-01 13:06:14 +0600

    Framer Motion Hook - useTransform()

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,18 +1,16 @@
-import { motion, useAnimate } from "framer-motion";
+import { motion, useDragControls } from "framer-motion";
 
 const Lesson = () => {
-const [scope, animate] = useAnimate();
 
-const handleClick = () => {
-animate(scope.current, {rotate: 45})
-}
-
+const controls = useDragControls();
   return (
     <div className="border border-red-500 size-[400px] flex flex-col justify-center items-center">
-      <div className="size-32 bg-indigo-500 rounded-lg flex flex-wrap gap-5 p-5 justify-center items-center"
-      ref={scope}
-      onClick={handleClick}
+      <div onPointerDown={(e) => controls.start(e)} className="w-32 h-6 size-32 bg-green-500 rounded-lg flex flex-wrap gap-5 p-5 justify-center items-center"
       ></div>
+      <motion.div className="size-32 bg-indigo-500 rounded-lg flex flex-wrap gap-5 p-5 justify-center items-center"
+      drag="x"
+      dragControls={controls}
+      ></motion.div>
     </div>
   );
 };
