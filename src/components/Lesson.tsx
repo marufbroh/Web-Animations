31f8975800a6f6d1286f9c453d57c72ce1087413
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-10-01 14:40:12 +0600

    Framer Motion Hook - useMotionValue()

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,15 +1,15 @@
-import { motion, useDragControls } from "framer-motion";
+import { motion, useMotionValue, useTransform } from "framer-motion";
 
 const Lesson = () => {
+  const x = useMotionValue(0);
 
-const controls = useDragControls();
+  const opacity = useTransform(x, [0, 400], [1, 0])
   return (
     <div className="border border-red-500 size-[400px] flex flex-col justify-center items-center">
-      <div onPointerDown={(e) => controls.start(e)} className="w-32 h-6 size-32 bg-green-500 rounded-lg flex flex-wrap gap-5 p-5 justify-center items-center"
-      ></div>
-      <motion.div className="size-32 bg-indigo-500 rounded-lg flex flex-wrap gap-5 p-5 justify-center items-center"
-      drag="x"
-      dragControls={controls}
+      <motion.div
+        className="size-32 bg-indigo-500 rounded-lg flex flex-wrap gap-5 p-5 justify-center items-center"
+        style={{ x, opacity }}
+        drag="x"
       ></motion.div>
     </div>
   );
