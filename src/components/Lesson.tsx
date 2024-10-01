e9f7eb0ed1255c611d45b0fe954887b235db5f18
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-10-01 15:16:30 +0600

    Animation on exit using AnimatePresence

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,18 +1,48 @@
-import { motion, useMotionValueEvent, useScroll } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
+import { useState } from "react";
 
 const Lesson = () => {
-  const { scrollX, scrollXProgress, scrollY, scrollYProgress } = useScroll();
+  const [visible, setVisible] = useState(true);
 
-  useMotionValueEvent(scrollY, "change", (e) => {
-    console.log(e);
-  });
+  const box = {
+    hidden: { opacity: 0, scale: 0.1 },
+    visible: {
+      opacity: 1,
+      scale: 1,
+      transition: {
+        duration: 2,
+      },
+    },
+    exit: {
+      opacity: 0,
+      scale: 0.5,
+      y: 100,
+      transition: {
+        duration: 2,
+      },
+    },
+  };
 
-  useMotionValueEvent(scrollYProgress, "change", (e) => {
-    console.log("progress", e);
-  });
   return (
     <div className="border border-red-500 size-[400px] flex flex-col justify-center items-center">
-      <motion.div className="size-32 bg-indigo-500 rounded-lg flex flex-wrap gap-5 p-5 justify-center items-center"></motion.div>
+      <motion.button
+        layout
+        className="bg-green-500 p-1 rounded"
+        onClick={() => setVisible(!visible)}
+      >
+        Toggle
+      </motion.button>
+      <AnimatePresence>
+        {visible && (
+          <motion.div
+            className="size-32 bg-indigo-500 rounded-lg flex flex-wrap gap-5 p-5 justify-center items-center"
+            variants={box}
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+          ></motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
