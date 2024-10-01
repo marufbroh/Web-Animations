89baca4661901074ce9b7ba6a7c5d6d2504a72b0
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-10-01 12:27:18 +0600

    Framer Motion Hook - useInView() and inView

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,13 +1,19 @@
-import { motion, useCycle } from "framer-motion";
+import { motion, useInView } from "framer-motion";
+import { useRef } from "react";
 
 const Lesson = () => {
-  const [x, cycle] = useCycle(0, 100, -100);
+const ref = useRef(null);
+
+const inView = useInView(ref, {once: true});
+
+// console.log(inView);
+
 
   return (
     <div className="border border-red-500 size-[400px] flex flex-col justify-center items-center">
       <motion.div className="size-32 bg-indigo-500 rounded-lg flex flex-wrap gap-5 p-5 justify-center items-center"
-      animate={{x: x}}
-      onTap={() => cycle()}
+      ref={ref}
+      animate={inView? {opacity: 1, x:0} : {opacity: 0, x: -500}}
       ></motion.div>
     </div>
   );
