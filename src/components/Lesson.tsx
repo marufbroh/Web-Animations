e53d5b548106741cb8f0ef84962728837388fbc4
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-10-01 14:57:34 +0600

    Framer Motion Hook - useScroll()

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,16 +1,18 @@
-import { motion, useMotionValue, useTransform } from "framer-motion";
+import { motion, useMotionValueEvent, useScroll } from "framer-motion";
 
 const Lesson = () => {
-  const x = useMotionValue(0);
+  const { scrollX, scrollXProgress, scrollY, scrollYProgress } = useScroll();
 
-  const opacity = useTransform(x, [0, 400], [1, 0])
+  useMotionValueEvent(scrollY, "change", (e) => {
+    console.log(e);
+  });
+
+  useMotionValueEvent(scrollYProgress, "change", (e) => {
+    console.log("progress", e);
+  });
   return (
     <div className="border border-red-500 size-[400px] flex flex-col justify-center items-center">
-      <motion.div
-        className="size-32 bg-indigo-500 rounded-lg flex flex-wrap gap-5 p-5 justify-center items-center"
-        style={{ x, opacity }}
-        drag="x"
-      ></motion.div>
+      <motion.div className="size-32 bg-indigo-500 rounded-lg flex flex-wrap gap-5 p-5 justify-center items-center"></motion.div>
     </div>
   );
 };
