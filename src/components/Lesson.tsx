388071ca053ba68f8b0b2078d715601044474fa9
Tamim Iqbal Maruf <marufmd775@gmail.com>
2024-10-01 12:59:49 +0600

    Framer Motion Hook - useAnimate()

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,20 +1,18 @@
-import { motion, useInView } from "framer-motion";
-import { useRef } from "react";
+import { motion, useAnimate } from "framer-motion";
 
 const Lesson = () => {
-const ref = useRef(null);
+const [scope, animate] = useAnimate();
 
-const inView = useInView(ref, {once: true});
-
-// console.log(inView);
-
+const handleClick = () => {
+animate(scope.current, {rotate: 45})
+}
 
   return (
     <div className="border border-red-500 size-[400px] flex flex-col justify-center items-center">
-      <motion.div className="size-32 bg-indigo-500 rounded-lg flex flex-wrap gap-5 p-5 justify-center items-center"
-      ref={ref}
-      animate={inView? {opacity: 1, x:0} : {opacity: 0, x: -500}}
-      ></motion.div>
+      <div className="size-32 bg-indigo-500 rounded-lg flex flex-wrap gap-5 p-5 justify-center items-center"
+      ref={scope}
+      onClick={handleClick}
+      ></div>
     </div>
   );
 };
